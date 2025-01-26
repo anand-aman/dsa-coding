4d35c1cf8b176b1c53159376b460d7e5cf722b92
anand-aman <itsamananand@gmail.com>
2025-01-26 22:49:46 +0530

    leetcode update

--- src/LeetCode/Solution.java
+++ src/LeetCode/Solution.java
@@ -1,33 +1,32 @@
 package LeetCode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    // https://leetcode.com/problems/course-schedule-ii/
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adjList1 = new ArrayList<>();
         for(int i=0; i<numCourses; i++){
             adjList1.add(new ArrayList<>());
         }
+
         for(int i=0; i<prerequisites.length; i++){
             int j = prerequisites[i][0];
             int k = prerequisites[i][1];
             adjList1.get(j).add(k);
         }
+
         if(detectCycle(adjList1, numCourses))
             return new int[0];
 
-        Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[numCourses];
+        ArrayList<Integer> itr = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        for(int i=0; i<adjList1.size(); i++){
+        for(int i=adjList1.size()-1; i>=0; i--){
             ArrayList<Integer> list = adjList1.get(i);
+            System.out.println(i+" -> "+ list);
             if(list.isEmpty()){
-                System.out.println(i);
-                q.add(i);
-                visited[i]=true;
+                itr.add(i);
             }
         }
 
@@ -38,38 +37,34 @@ class Solution {
         for(int i=0; i<prerequisites.length; i++){
             int j = prerequisites[i][0];
             int k = prerequisites[i][1];
-
             adjList2.get(k).add(j);
         }
-        ArrayList<Integer> ans = new ArrayList<>();
 
-
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            boolean flag = false;
-            for(int pr : adjList1.get(curr)){
-                if(q.contains(pr)){
-                    q.add(curr);
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag)
+        for(int item : itr){
+            if(visited[item])
                 continue;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(item);
+            visited[item]=true;
+            while(!q.isEmpty()){
+                int curr = q.poll();
+                ans.add(curr);
 
-            ans.add(curr);
-
-            ArrayList<Integer> courses = adjList2.get(curr);
-            for(int course : courses){
-                if(!visited[course]){
-                    q.add(course);
-                    visited[course]=true;
+                ArrayList<Integer> courses = adjList2.get(curr);
+                for(int course : courses){
+                    if(!visited[course]){
+                        q.add(course);
+                        visited[course]=true;
+                    }
                 }
             }
         }
-        int[] ansArray = new int[ans.size()];
-        for(int i=0; i<ans.size(); i++){
-            ansArray[i]=ans.get(i);
+
+        int[] ansArray = new int[(ans.size())];
+        int j=0;
+        while(j<ans.size()){
+            ansArray[j]=ans.get(j);
+            j++;
         }
         return ansArray;
 
@@ -78,19 +73,33 @@ class Solution {
 
     private boolean detectCycle(ArrayList<ArrayList<Integer>> adjList, int n){
         boolean[] visited = new boolean[n];
-        visited[0]=true;
-        return isCycle(adjList, visited, 0);
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                // System.out.print("\nComponent: "+i+" --> ");
+                Set<Integer> path = new HashSet<>();
+                path.add(i);
+                visited[i]=true;
+                if(isCycle(adjList, visited, i, path))
+                    return true;
+            }
+        }
+        return false;
     }
 
-    private boolean isCycle(ArrayList<ArrayList<Integer>> adjList, boolean[] visited, int node){
+    private boolean isCycle(ArrayList<ArrayList<Integer>> adjList, boolean[] visited, int node, Set<Integer> path){
         ArrayList<Integer> list = adjList.get(node);
+        path.add(node);
         boolean ans = false;
         for(int i : list){
-            if(visited[i])
+            // System.out.print(node+", ");
+            if(visited[i] && path.contains(i))
                 return true;
             visited[i]=true;
-            ans = ans || isCycle(adjList, visited, i);
+            Set<Integer> newPath = new HashSet<>(path);
+            newPath.add(i);
+            ans = ans || isCycle(adjList, visited, i, newPath);
         }
+
         return ans;
     }
 
